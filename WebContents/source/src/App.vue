c002b4e09ac8053bac4e3614eacf5f98f1a00bab
sntw-5569 <ore.no.yeah@gamil.com>
2019-04-28 16:33:16 +0900

    add fuustagram main frames.

---
+++ WebContents/source/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <div class="base-layer n-type">
+      <header-bar />
+      <main-contents />
+      <button @click="dummyPush()">Push Me.</button>
+      <footer-menu />
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderBar from './components/HeaderBar'
+import MainContents from './components/MainContents'
+import FooterMenu from './components/FooterMenu'
+
+export default {
+  name: 'app',
+  components: {
+    HeaderBar,
+    MainContents,
+    FooterMenu
+  },
+  data () {
+    return {
+      errorMessage: ""
+    }
+  },
+  methods: {
+    dummyPush: function() {
+      if (document.getElementById('m-notice')) {
+        return
+      }
+      this.errorMessage = "Notification Popup."
+      document.body.insertAdjacentHTML("afterbegin",
+        '<div id="m-notice" class="n-notify n-center n-notify--fixed">'
+        + this.errorMessage
+        +'</div>');
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma/bulma.sass';
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.base-layer {
+  position: relative;
+}
+
+.n-notify {
+  height: 80px;
+}
+.-no-disp {
+  display: none;
+}
+.-inline {
+  display: inline;
+}
+</style>
---
+++ WebContents/source/src/components/HeaderBar.vue
@@ -0,0 +1,125 @@
+<template>
+  <header class="header-bar">
+    <nav class="level is-flex">
+      <div class="level-left title-logo">
+        <figure class="image is-4by1">
+          <a href="/" class="is-inline">
+            <img src="../assets/fuusta_logo.png" href="/">
+          </a>
+        </figure>
+      </div>
+      <div class="level-right is-flex">
+        <div class="level-item control has-icons-left">
+          <input class="input serch-input" type="text" placeholder="keyword">
+            <span class="icon is-small is-left input-innner">
+              <font-awesome-icon icon="search" class="input-innner-icon"/>
+          </span>
+        </div>
+        <div class="level-item head-icon">
+          <font-awesome-icon icon="sync-alt" />
+        </div>
+        <div class="level-item head-icon">
+          <font-awesome-icon icon="arrow-alt-circle-up" />
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "haeder-bar",
+  data() {
+    return {
+      errorMessage: "",
+      isFilterd: false,
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header-bar {
+  position: sticky;
+  top: 0;
+  overflow: hidden;
+  background-color: white;
+  // border-bottom: solid 1px rgb(150, 150, 150);
+  box-shadow: 1px 2px 3px 0px #bbb;
+  z-index: 1;
+}
+.level-item {
+  margin: 0;
+}
+.input {
+    color: darkred;
+    background-color: rgb(238, 238, 238);
+    font-size: small;
+}
+.input-innner-icon {
+  color: slategray;
+}
+
+@media screen and (min-width : 300px){
+  nav.level {
+    padding: 0;
+    margin: 0;
+  }
+  .level-right {
+    width: 60%;
+    margin: 0;
+    div {
+      padding: 2px;
+    }
+  }
+  .input {
+    width: 100%;
+  }
+  div.control {
+    width: 80%;
+  }
+
+  .title-logo {
+    width: 30%;
+    img {
+      width: 100%;
+    }
+  }
+  .head-icon {
+    font-size: 22px;
+  }
+}
+
+@media screen and (min-width : 720px){
+  div.level-right {
+    padding-top: 5px;
+  }
+  .title-logo {
+    width: 30%;
+    max-width: 200px
+    img {
+      width: 100%;
+      margin: 10px;
+    }
+  }
+  .head-icon {
+    font-size: 35px;
+  }
+  .level-right {
+    width: 60%;
+    margin: 0;
+    div {
+      padding: 2px;
+    }
+  }
+  .input {
+    width: 100%;
+  }
+  div.control {
+    width: 80%;
+  }
+}
+
+</style>
